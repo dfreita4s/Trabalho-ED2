02d6e1671b409082bf5785d29c9c6190d8907647
dfreita4s <danifp19@gmail.com>
2021-11-18 10:43:38 -0300

    entendendo o codigo

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@ int main(int argc, char const *argv[])
 {
     // Diretório completo para funcionar o Debug
     string caminhoArquivo;
-    caminhoArquivo = "";
+    caminhoArquivo = "/data/tiktok_app_reviews.csv";
     
     Lista *listaReview = new Lista(caminhoArquivo);
 
     listaReview->obterReviews();
 
-    //listaReview->listarTodas();
+    // listaReview->listarTodas();
+
+    listaReview->acessaRegistro(2);
 
     delete listaReview;
 
