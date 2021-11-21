fa6432282352c20c84a031b2f9646cefdd032809
dfreita4s <danifp19@gmail.com>
2021-11-21 02:55:54 -0300

    algumas mudanças

--- src/lista.cpp
+++ src/lista.cpp
@@ -10,7 +10,7 @@ Lista::Lista(const std::string &caminhoArquivo)
 
 Lista::~Lista()
 {
-    while(this->raiz != nullptr)
+    while (this->raiz != nullptr)
     {
         Review *novaRaiz = raiz->obterProximo();
         delete this->raiz;
@@ -19,7 +19,6 @@ Lista::~Lista()
     this->arquivo.close();
 }
 
-
 bool Lista::abrirArquivo(const std::string &caminhoArquivo)
 {
     try
@@ -27,28 +26,31 @@ bool Lista::abrirArquivo(const std::string &caminhoArquivo)
         this->arquivo.open(caminhoArquivo, std::ios::in);
         return true;
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
-        std::cerr << "Houve um erro ao abrir o arquivo!" << std::endl << e.what() << '\n';
+        std::cerr << "Houve um erro ao abrir o arquivo!" << std::endl
+                  << e.what() << '\n';
     }
     return false;
 }
 
-int Lista::obterTam(){
-    int size=0;
-    if(this->arquivo.is_open())
+int Lista::obterTam()
+{
+    int size = 0;
+    if (this->arquivo.is_open())
     {
-        if(this->obterRaiz() != nullptr)
+        if (this->obterRaiz() != nullptr)
         {
             Review *No = this->raiz;
-            while(No->obterProximo() != nullptr)
+            while (No->obterProximo() != nullptr)
             {
                 size++;
                 No = No->obterProximo();
             }
             return size;
         }
-        else{
+        else
+        {
             return 0;
         }
     }
@@ -57,63 +59,63 @@ int Lista::obterTam(){
 
 bool Lista::obterReviews()
 {
-    if(this->arquivo.is_open())
+    if (this->arquivo.is_open())
     {
         std::string linha;
 
         getline(this->arquivo, linha); //Le cabecalho do arquivo
 
         Review *ultimo = nullptr;
-        
+
         getline(this->arquivo, linha);
 
-        int k=0; // contar registros
+        int k = 0; // contar registros
 
-        while(!arquivo.eof() && linha != "")
+        while (!arquivo.eof() && linha != "")
         {
             /* Trata os registros que estão com '\n'
                Verifica se a última informação é do tipo ':SS' */
-            
-            while(!(isdigit(linha[linha.length()-1]) && (isdigit(linha[linha.length()-2])) && (linha[linha.length()-3]==':')))
+
+            while (!(isdigit(linha[linha.length() - 1]) && (isdigit(linha[linha.length() - 2])) && (linha[linha.length() - 3] == ':')))
             {
                 std::string aux = linha;
                 getline(this->arquivo, linha);
                 aux.append(linha);
                 linha = aux;
             }
-            
+
             k++;
 
             int pos;
             pos = linha.find(",");
-            std::string id = linha.substr(3, pos-3); // Inicio em 3 para retirar 'gp:'
-            linha = linha.substr(pos+1, linha.length());
-            
+            std::string id = linha.substr(3, pos - 3); // Inicio em 3 para retirar 'gp:'
+            linha = linha.substr(pos + 1, linha.length());
+
             pos = linha.find_last_of(",");
-            std::string data = linha.substr(pos+1); // Obter a Data
+            std::string data = linha.substr(pos + 1); // Obter a Data
             linha = linha.substr(0, pos);
 
             pos = linha.find_last_of(",");
-            std::string versao = linha.substr(pos+1); // Obter a versão
-            if(versao.length() == 0)
+            std::string versao = linha.substr(pos + 1); // Obter a versão
+            if (versao.length() == 0)
                 versao = "NaN";
             linha = linha.substr(0, pos);
 
             pos = linha.find_last_of(",");
-            int upvotes = stod(linha.substr(pos+1)); // Obter upvotes
+            int upvotes = stod(linha.substr(pos + 1)); // Obter upvotes
             linha = linha.substr(0, pos);
 
             std::string texto = linha; // Obter o comentário
-            if(texto.find("\"") != -1)
-                texto = texto.substr(1, texto.length()-2); // Caso comece com esteja entre "", retirá-los
+            if (texto.find("\"") != -1)
+                texto = texto.substr(1, texto.length() - 2); // Caso comece com esteja entre "", retirá-los
 
             Review *review = new Review(id, texto, upvotes, versao, data); // Cria o Review
-            this->inserirReview(review, ultimo); // Insere na lista
-            ultimo = review; // Atualiza ponteiro do último Review para o atual.
-            
+            this->inserirReview(review, ultimo);                           // Insere na lista
+            ultimo = review;                                               // Atualiza ponteiro do último Review para o atual.
+
             getline(this->arquivo, linha);
         }
-        std::cout << k-1 << " de registros foram importados com sucesso." << std::endl;
+        std::cout << k - 1 << " de registros foram importados com sucesso." << std::endl;
         criarArquivoBinario();
         return true;
     }
@@ -121,20 +123,22 @@ bool Lista::obterReviews()
     return false;
 }
 
-bool Lista::criarArquivoBinario(){
+bool Lista::criarArquivoBinario()
+{
 
-    std::fstream arq("./data/tiktok.bin", std::ios::out | std::ios::binary);
-    Review * raiz = obterRaiz();
+    std::fstream arq("./data/tiktok_app_reviews.bin", std::ios::out | std::ios::binary); //cria arqv .bin
+    Review *raiz = obterRaiz();
     int k = obterTam();
-    if(arq.is_open())
-    {   
-        for(int i=0; i<=k; i++){
+    if (arq.is_open())
+    {
+        for (int i = 0; i <= k; i++)
+        {
             // std::string id = raiz->obterID();
-            arq.write(reinterpret_cast<const char*>(raiz), (sizeof(Review)));
-            raiz=raiz->obterProximo();
+            arq.write(reinterpret_cast<const char *>(raiz), (sizeof(Review)));
+            raiz = raiz->obterProximo();
         }
         std::cout << "criou arquivo!" << std::endl;
-    } 
+    }
     else
         std::cout << "Deu errado";
 }
@@ -149,7 +153,7 @@ void Lista::inserirReview(Review *rev, Review *ultimo)
 
 Review *Lista::obterRaiz()
 {
-    if(!(this->raiz == nullptr))
+    if (!(this->raiz == nullptr))
         return this->raiz;
     else
     {
@@ -161,12 +165,12 @@ Review *Lista::obterRaiz()
 // Listar todos Reviews presentes na Lista.
 void Lista::listarTodas()
 {
-    if(this->arquivo.is_open())
+    if (this->arquivo.is_open())
     {
-        if(this->obterRaiz() != nullptr)
+        if (this->obterRaiz() != nullptr)
         {
             Review *No = this->raiz;
-            while(No->obterProximo() != nullptr)
+            while (No->obterProximo() != nullptr)
             {
                 std::cout << std::endl;
                 No->exibeRegistro();
@@ -183,12 +187,12 @@ void Lista::listarTodas()
 // Esboço da função para acessar o k-ésimo registro
 bool Lista::acessaRegistro(int k)
 {
-    if(this->obterRaiz() != nullptr)
+    if (this->obterRaiz() != nullptr)
     {
         Review *No = this->raiz;
-        for(int i=0; ((No != nullptr) && (i<=k)); i++)
+        for (int i = 0; ((No != nullptr) && (i <= k)); i++)
         {
-            if(i==k)
+            if (i == k)
             {
                 No->exibeRegistro();
                 return true;
@@ -196,7 +200,8 @@ bool Lista::acessaRegistro(int k)
             else
                 No = No->obterProximo();
         }
-        std::cout << std::endl << "Registro não encontrado." << std::endl;
+        std::cout << std::endl
+                  << "Registro não encontrado." << std::endl;
         return false;
     }
     else
@@ -206,51 +211,75 @@ bool Lista::acessaRegistro(int k)
     }
 }
 
-void Lista::testeImportacao(int resp){
-    
-    std::string caminhoArquivo = "./data/tiktok.bin";
-    std::ifstream dadosBin;
-    std::string* linhaInteira = new std::string [3646475];
-    dadosBin.open(caminhoArquivo, std::ios_base::in | std::ios_base::binary);
-    if(resp == 1){
-        if(!dadosBin.is_open()){
-            std::cout << "Arquivo não foi aberto"; 
-        }else{
-            int N = 10;
-            for(int i = 0; i<=N; i++){
-            dadosBin >> linhaInteira[2]; 
-            std::cout << linhaInteira[2] << "/n" << "\n";
-            }
-        }
-        //std::cout << linhaInteira[10];
-    }
-    else if(resp == 2){
-    std::fstream dadosSaida;
-    dadosSaida.open("dadosSaida.txt", std::ios_base::out | std::ios_base::app);
-    // executa a saida de 100 valores do .bin para o .txt
-    dadosSaida.close();
-    }
-    else
-    std::cout << "Opção Inválida";
-}
+void Lista::testeImportacao()
+{
 
-void Lista::usaListaImportacao(){
+    // std::string caminhoArquivo = "./data/tiktok.bin";
+    // std::ifstream dadosBin;
+    // std::string *linhaInteira = new std::string[3646475];
+    // dadosBin.open(caminhoArquivo, std::ios_base::in | std::ios_base::binary);
     int resp, N = 0;
-    std::cout<<" Deseja exibir a saida no console ou salva-la em um arquivo texto? 1 para no console 2 para salvar. " << std::endl;
-    std::cin>>resp;
-    if(resp == 1){
+    int k = obterTam();
+    std::cout << " Deseja exibir a saida no console ou salva-la em um arquivo texto? 1 para no console 2 para salvar. " << std::endl;
+    std::cin >> resp;
+    if (resp == 1)
+    {
         //escolha saida em console N = 10
         N = 10;
-        for(int i = N; i>=0; i--){
-         acessaRegistro(rand() % 3646475 + 0);
+        for (int i = N; i >= 0; i--)
+        {
+            acessaRegistro(rand() % k + 0);
         }
-    }else if(resp == 2){
-        std::ofstream dadosSaida;
-        dadosSaida.open("dados.txt", std::ios_base::out | std::ios_base::app);
-        N = 100;
-
-    }else{
-        std::cout<<"Por favor digite uma resposta válida!"<<std::endl;
-        usaListaImportacao();
     }
+    else if (resp == 2)
+    {
+        // executa a saida de 100 valores do .bin para o .txt
+        N = 100;
+        std::fstream dadosSaida;
+        dadosSaida.open("dadosSaida.txt", std::ios_base::out | std::ios_base::app);
+        if (dadosSaida.is_open())
+        {
+
+            for (int i = N; i >= 0; i--)
+            {
+                
+            }
+        }
+        else
+        {
+            std::cout << "Erro ao abrir arquivo!" << std::endl;
+        }
+        dadosSaida.close();
+    }
+    else
+        std::cout << "Opção Inválida";
 }
+
+// void Lista::usaListaImportacao()
+// {
+//     int resp, N = 0;
+
+//     if (resp == 1)
+//     {
+//         //escolha saida em console N = 10
+//         N = 10;
+//         for (int i = N; i >= 0; i--)
+//         {
+//             acessaRegistro(rand() % 3646475 + 0);
+//         }
+//     }
+//     else if (resp == 2)
+//     {
+//         std::ofstream dadosSaida;
+//         dadosSaida.open("dados.txt", std::ios_base::out | std::ios_base::app);
+//         N = 100;
+//         for (int i = N; i >= 0; i--)
+//         {
+//         }
+//     }
+//     else
+//     {
+//         std::cout << "Por favor digite uma resposta válida!" << std::endl;
+//         usaListaImportacao();
+//     }
+// }
