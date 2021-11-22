2f84b8e65b2a4228b6d499a2923ed1f18339680b
dfreita4s <danifp19@gmail.com>
2021-11-22 14:52:45 -0300

    Tudo ok

--- inc/lista.h
+++ inc/lista.h
@@ -4,25 +4,24 @@
 
 class Lista
 {
-    public:
-        Review *raiz;
-        std::ifstream arquivo;
+public:
+    Review *raiz;
+    std::ifstream arquivo;
 
-        Lista();
-        Lista(const std::string&);
-        ~Lista();
+    Lista();
+    Lista(const std::string &);
+    ~Lista();
 
-        int obterTam();
-        bool obterReviews();
-        Review *obterRaiz();
-        void listarTodas();
-        // void acessaRegistro(int);
-        void inserirReview(Review *, Review *);
-        // void testeImportacao();
-        bool criarArquivoBinario();
-        void usaListaImportacao();
+    int obterTam();
+    bool obterReviews();
+    Review *obterRaiz();
+    void listarTodas();
 
-    protected:
-        bool abrirArquivo(const std::string&);
-        
+    void inserirReview(Review *, Review *);
+
+    bool criarArquivoBinario();
+    void usaListaImportacao();
+
+protected:
+    bool abrirArquivo(const std::string &);
 };
