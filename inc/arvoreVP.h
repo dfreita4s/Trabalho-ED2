ec9205c9283131f9016adca3173598ed2dc382e1
Aloizio02 <aloizio@ice.ufjf.br>
2022-01-17 19:17:44 -0300

    modifica AVP

--- inc/arvoreVP.h
+++ inc/arvoreVP.h
@@ -31,6 +31,15 @@ public:
     void imprimirNo(NoVP* no, bool);
     void buscaNo(arvoreVP *, std::string);
 
+    //novas funções recursivas para a árvore vermelho e preta
+    void insere_caso1(NoVP *);
+    void insere_caso2(NoVP *);
+    void insere_caso3(NoVP *);
+    void insere_caso4(NoVP *);
+    void novoRotacionaDir(NoVP *);
+    void novoRotacionaEsq(NoVP *);
+    void novoRotDuplaEsq(NoVP *);
+    void novoRotDuplaDir(NoVP *);
 };
 
 #endif
